2d60acd01c2f38b5db2618130314b09188e33c51
Ederson Dias <edersond@gmail.com>
2020-01-07 16:43:12 -0300

    Updated plot1.R
    updated plot1.R to insert comments

--- plot1.R
+++ plot1.R
@@ -1,13 +1,19 @@
-library(tidyverse)
+#loads data
 source("data_load.R")
+
+#sets device to png, using given sizes
 png(filename = "plot1.png",
     width = 480, 
     height = 480, 
     units = "px")
+
+#plots histogram
 hist(as.numeric(data$Global_active_power),
      col = "red",
      main = "Global Active Power",
      xlab = "Global Active Power (kilowatts)",
      ylim = c(0,1200))
+
+#turns of device (save plot)
 dev.off()
 
